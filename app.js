b77c2dc8ba9e165bd6bb3bd7f6c5724e8cac90e7
Adam Gamba <43792793+adamgamba@users.noreply.github.com>
2021-01-15 16:38:20 -0500

    Work on contacts, programming projects, and interests
    start styling site

---
+++ app.js
@@ -0,0 +1,17 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+var exampleEl = document.getElementById('example')
+var tooltip = new bootstrap.Tooltip(exampleEl, options)
+var $copyBtns = $("#btnCopy");
+var copyClient = new ZeroClipboard($copyBtns);
+$copyBtns.tooltip({
+    trigger: 'manual',
+    container: 'body'
+});
