5dac1fcd766af6c33668859aaf3c421c2aa77c5b
Adam Gamba <43792793+adamgamba@users.noreply.github.com>
2021-01-16 21:00:39 -0500

    Finish Styling
    Finish styles and add images. Still need to add content

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Script: Show tooltips on contact buttons
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
